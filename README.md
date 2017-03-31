59836ca1670a75bc8c547245f109ded50afd1045
Aaron Burrows <me@aaronburrows.com>
2017-03-31 16:33:25 -0500

    complete Hello World with some fixes for NPM.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # preact-ts
+
+Hello World in Preact with TypeScript taken from https://medium.com/@shakyShane/hello-world-with-preact-jsx-typescript-6d70cf2ebf01
+
---
+++ src/app.js
@@ -0,0 +1,4 @@
+"use strict";
+var preact_1 = require("preact");
+var HelloWorld_1 = require("./HelloWorld");
+preact_1.render(React.createElement(HelloWorld_1.default, { name: "World" }), document.querySelector('#app'));
